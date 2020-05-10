646a0451026d59f0e20d852a8db0d731332db697
Sam Hodgkinson <samhodg222@gmail.com>
2020-05-10 18:01:28 +0100

    Updated features
    Added lines which connect nearby particles.
    
    Added basic particle collision with other particles and walls

--- particle.js
+++ particle.js
@@ -1,26 +1,39 @@
 class Particle {
-    constructor () {
+    constructor(){
         this.x = random(1,width);
         this.y = random(1,height);
-        this.radius = 10;
-        this.xSpeed = random(-2,2);
-        this.ySpeed = random(-2,2);
+        this.radius = random(10,15);
+        this.xSpeed = random(-3,2.5);
+        this.ySpeed = random(-2,2.5);
+        this.alpha = '#e74c3c';
     }
-
-
+    
     createParticle(){
-        noStroke();
-        fill('#e74c3c');
+        fill(this.alpha);
         circle(this.x,this.y,this.radius);
     }
     moveParticle(){
         this.x += this.xSpeed;
         this.y += this.ySpeed;
-        if(this.x == width || this.x == 0) {
+        if(this.x > width || this.x < 0) {
             this.xSpeed*=-1;
-        } else if (this.y == height || this.h == 0) {
+        } else if (this.y > height || this.y < 0) {
             this.ySpeed*=-1;
         }
     }
-  
+
+    linkWithLines(particles) { 
+        particles.forEach(element => {
+            let dis = dist(this.x,this.y,element.x,element.y);
+            if(dis<90) {
+                stroke(0);
+                line(this.x,this.y,element.x,element.y);
+              } 
+            if(dis < 10){
+                this.xSpeed *= -1;
+                element.xSpeed *= -1;    
+            }   
+        });
+    }
+
 }
--- sketch.js
+++ sketch.js
@@ -2,18 +2,33 @@
 
 
 let particles = [];
+let limit;
 function setup() {
     createCanvas(1200,600);
-    frameRate(30);
-    for(i = 0; i < 50; i ++){
+    frameRate(60);
+    limit = 90;
+    for(i = 0; i < limit; i ++){
         particles.push(new Particle());
     }
+    console.log('No. Particles: ' + floor(limit).toString());
+    let stopButton = createButton('Stop');
+    stopButton.position(10, 10);
+    stopButton.mousePressed(stopAnimation);
+    let startButton = createButton('Play');
+    startButton.position(50, 10);
+    startButton.mousePressed(playAnimation);
 }
 function draw() {
     background(100);
-    console.log('drawCalled');
     for(i = 0; i < particles.length; i++){
         particles[i].createParticle();
         particles[i].moveParticle();
+        particles[i].linkWithLines(particles.slice(i));
     }
+}
+function stopAnimation() {
+    frameRate(0);
+}
+function playAnimation() {
+    frameRate(60);
 }
